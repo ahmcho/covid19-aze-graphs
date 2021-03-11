af67d7529f4e8d24f8ce977a98a4bf2f292d90ee
ahmcho <mail@ahmcho.com>
2021-03-11 19:07:01 +0400

    Add react-bootstrap

--- src/App.js
+++ src/App.js
@@ -1,47 +1,67 @@
 import {useEffect, useState} from 'react';
-//Components
-import InfectedAll from './InfectedAll';
-import InfectedToday from './InfectedToday';
-import RecoveredAll from './RecoveredAll';
-import DeathsAll from  './DeathsAll';
-import TestsAll from './TestsAll';
-import TestsToday from './TestsToday';
-import DeathsToday from './DeathsToday';
-import RecoveredToday from './RecoveredToday';
-import ActiveCases from './ActiveCases';
+import moment from 'moment';
+import Jumbotron from 'react-bootstrap/Jumbotron';
+import Container from 'react-bootstrap/Container';
+import Spinner from 'react-bootstrap/Spinner';
+import Card from 'react-bootstrap/Card';
+import CardGroup from 'react-bootstrap/CardGroup';
+import Badge from 'react-bootstrap/Badge';
+import Chart from './Chart';
+
+//Titles
+import chartTitles from './chartTitles';
+import cardTitles from './cardTitles';
 import axios from 'axios';
 
 const App = () => {
   const [chartData, setChartData] = useState([]);
 
+  
   const fetchData = async () => {
     try {
       let response = await axios.get('https://covid19-azerbaijan.ahmcho.com/api/cases/all');
       setChartData(response.data);
-      localStorage.setItem('data', JSON.stringify(response.data));
     } catch (error) {
       console.error(error);
     }
   }
+
   useEffect(() =>{
-    if(localStorage.getItem('data') === null){
-      fetchData();
-    } else {
-      setChartData(JSON.parse(localStorage.getItem('data')));
-    }
+    fetchData();
   },[]);
 
   return(
     <>
-      <InfectedAll data={chartData} />
-      <InfectedToday data={chartData} />
-      <RecoveredAll data={chartData} />
-      <RecoveredToday data={chartData} />
-      <ActiveCases data={chartData} />
-      <DeathsAll data={chartData} />
-      <DeathsToday data={chartData} />
-      <TestsAll data={chartData} />
-      <TestsToday data={chartData} />
+      <Container className="p-3">
+        <Jumbotron>
+          <h1 className="text-center">Azərbaycan üzrə COVID-19 Statistikası</h1> 
+        </Jumbotron>
+      
+    {chartData.length === 0 ? (  <Spinner animation="border" size="md" variant="primary" /> ) : (
+        <>
+        <CardGroup>
+        {
+          cardTitles.map((item,index) => (
+            <Card key={index}>
+              <Card.Body>
+                <Card.Title>{item.title}</Card.Title>
+                <Card.Text as="h4">
+                  <Badge pill variant="primary">{chartData[chartData.length-1][item.type]}</Badge>
+                </Card.Text>
+              </Card.Body>
+            </Card>
+          ))
+        }
+        </CardGroup>
+        <small className="text-muted">Son yenilənmə tarixi {moment(chartData[chartData.length-1].report_date).format('DD.MM.YYYY')}</small>
+        {
+          chartTitles.map((item,index) => (
+              <Chart data={chartData} lineName={item.title} lineType={item.type} line_dataKey={item.type} key={index}/>
+          ))
+        }
+        </>
+    )}
+    </Container>
     </>
   )
 }
