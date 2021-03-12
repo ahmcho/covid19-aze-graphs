f71ffd01b1910e903e5c906d985ab59d2be61739
ahmcho <mail@ahmcho.com>
2021-03-12 15:27:24 +0400

    Remove unnecessary comments

--- src/App.js
+++ src/App.js
@@ -1,33 +1,43 @@
+//Boilerplate and installed packages
 import {useEffect, useState} from 'react';
-import moment from 'moment';
 import Jumbotron from 'react-bootstrap/Jumbotron';
 import Container from 'react-bootstrap/Container';
 import Spinner from 'react-bootstrap/Spinner';
-import Card from 'react-bootstrap/Card';
-import CardGroup from 'react-bootstrap/CardGroup';
-import Badge from 'react-bootstrap/Badge';
-import Chart from './Chart';
-
-//Titles
-import chartTitles from './chartTitles';
-import cardTitles from './cardTitles';
+import Tabs from 'react-bootstrap/Tabs';
+import Tab from 'react-bootstrap/Tab';
 import axios from 'axios';
 
-const App = () => {
-  const [chartData, setChartData] = useState([]);
+//Components
+import Cases from './components/Cases/Cases';
+import Vaccines from './components/Vaccines/Vaccines';
 
+const App = () => {
+  const [caseData, setCaseData] = useState([]);
+  const [vaccineData, setVaccineData] = useState([]);
   
-  const fetchData = async () => {
+  //Get case data from API
+  const fetchCaseData = async () => {
     try {
       let response = await axios.get('https://covid19-azerbaijan.ahmcho.com/api/cases/all');
-      setChartData(response.data);
+      setCaseData(response.data);
+    } catch (error) {
+      console.error(error);
+    }
+  }
+
+  //Get vaccine data from API
+  const fetchVaccineData = async () => {
+    try {
+      let response = await axios.get('https://covid19-azerbaijan.ahmcho.com/api/vaccines/all');
+      setVaccineData(response.data);
     } catch (error) {
       console.error(error);
     }
   }
 
   useEffect(() =>{
-    fetchData();
+    fetchCaseData();
+    fetchVaccineData();
   },[]);
 
   return(
@@ -36,31 +46,18 @@ const App = () => {
         <Jumbotron>
           <h1 className="text-center">Azərbaycan üzrə COVID-19 Statistikası</h1> 
         </Jumbotron>
-      
-    {chartData.length === 0 ? (  <Spinner animation="border" size="md" variant="primary" /> ) : (
-        <>
-        <CardGroup>
-        {
-          cardTitles.map((item,index) => (
-            <Card key={index}>
-              <Card.Body>
-                <Card.Title>{item.title}</Card.Title>
-                <Card.Text as="h4">
-                  <Badge pill variant="primary">{chartData[chartData.length-1][item.type]}</Badge>
-                </Card.Text>
-              </Card.Body>
-            </Card>
-          ))
-        }
-        </CardGroup>
-        <small className="text-muted">Son yenilənmə tarixi {moment(chartData[chartData.length-1].report_date).format('DD.MM.YYYY')}</small>
-        {
-          chartTitles.map((item,index) => (
-              <Chart data={chartData} lineName={item.title} lineType={item.type} line_dataKey={item.type} key={index}/>
-          ))
-        }
-        </>
-    )}
+        {caseData.length === 0 || vaccineData.length === 0 ? (  <Spinner style={{ display: "flex",margin: "0 auto" }} animation="border" size="md" variant="primary" /> ) : (
+            <>
+              <Tabs defaultActiveKey="cases" fill>
+                <Tab eventKey="cases" title="Xəstəlik statistikası">
+                  <Cases data={caseData}/>
+                </Tab>
+                <Tab eventKey="vaccines" title="Vaksin statistikası">
+                  <Vaccines data={vaccineData} />
+                </Tab>
+              </Tabs>
+            </>
+        )}
     </Container>
     </>
   )
--- src/cardTitles.js
+++
@@ -1,9 +0,0 @@
-const cardTitles = [
-    { title: 'Sağalanların sayı', type: 'recovered_all'},
-    { title: 'Aktiv xəstə sayı', type: 'active_cases'},
-    { title: 'Yoluxanların sayı', type: 'infected_all'},
-    { title: 'Ölənlərin sayı', type: 'deaths_all'},
-    { title: 'Testlərin sayı', type: 'tests_all'}
-]
-
-export default cardTitles;
